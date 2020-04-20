8f1f67765a162f3734650b8d6508c3625774d92c
Techsider <54804321+TechsiderYT@users.noreply.github.com>
2020-04-20 20:03:25 +0200

    lol

--- handlers/backupmethods/load.js
+++ handlers/backupmethods/load.js
@@ -1,35 +1,39 @@
 module.exports = async (client, backupid, message, args, con) => {
     con.query("SELECT * FROM `backups` WHERE backupid = ? ", [backupid], async (error, result) => {
-        console.log(result);
+        //console.log(result);
         if(result[0]){
             //DELETING SERVER
-            /*
-            await message.guild.roles.cache.forEach(role =>{
-                if(role.name === "@everyone" || role.managed) return;
-                console.log("Deleting Role: " + role.name);
-                role.delete();
-            })*/
+            console.log(1);
+            for (let index = 0; index < message.guild.roles.cache.array().length; index++) {
+                console.log(2);
+                
+                let role = message.guild.roles.cache.array()[index];
+                console.log(2.1);
+                if(role.name != "@everyone" && !role.managed){
+                    console.log(2.2);
+                    console.log("Deleting Role: " + role.name);
+                    
+                    role.delete();
+                    await client.timeout(1000);
+                }
+                
+            }
+            console.log(3);
+            console.log(message.guild.roles.cache.array().length);          
+            console.log(4);
+           
+            for (let index = 0; index < message.guild.channels.cache.array().length; index++) {
+                console.log(5);
+                let channel = message.guild.channels.cache.array()[index];
+                console.log("Deleting Channel: " + channel.name);
+                channel.delete();
+                await client.timeout(1000);
+                console.log(6);
+            }
+            console.log(7);
 
-            for (let index = 0; index < message.guild.roles.cache.length; index++) {
-                let role = message.guild.roles.cache[index];
-                if(role.name === "@everyone" || role.managed) return;
-                console.log("Deleting Role: " + role.name);
-                role.delete();
-                await client.timeout(750);
-            }
-            /*
-            await message.guild.channels.cache.forEach(channel =>{
-                console.log("Deleting Channel: " + channel.name);
-                channel.delete();
-            })
-            */
-            for (let index = 0; index < message.guild.channels.cache.length; index++) {
-                let channel = message.guild.channels.cache[index];
-                console.log("Deleting Channel: " + channel.name);
-                channel.delete();
-                await client.timeout(750);
-            }
-            
+            await client.timeout(2000);
+
             //RESTORING SERVER
             con.backups.query("SELECT * FROM `backup_" + backupid + "_general`", [], async (error, information) => {
                 message.guild.setName(information[0].guildname);
@@ -41,7 +45,7 @@ module.exports = async (client, backupid, message, args, con) => {
                 if(error) throw error;
                 for (let index = 0; index < roles.length; index++) {
                     let role = roles[index];
-                    await client.timeout(750);
+                    await client.timeout(1000);
                     console.log("Creating role: " + role.name);
                     message.guild.roles.create({
                         data: {
@@ -58,13 +62,12 @@ module.exports = async (client, backupid, message, args, con) => {
 
             });
             
-            
             con.backups.query("SELECT * FROM `backup_" + backupid + "_channels`", [], async (error, channels) => {
                 if(error) throw error;
                 console.log(channels);
                 for (let index = 0; index < channels.length; index++) {
                     let channel = channels[index];
-                    await client.timeout(750);
+                    await client.timeout(1000);
                     console.log("Creating Channel: " + channel.name);
                     message.guild.channels.create(channel.name, {
                             type: channel.type,
@@ -77,7 +80,9 @@ module.exports = async (client, backupid, message, args, con) => {
                     );
 
                 }               
-            })           
+            })   
+            
+            
         }
     });
 }
